7076c2641ed4e9eb518e8a2f57909c2eb6983711
Burin Vatana <42647918+bvatan9684@users.noreply.github.com>
2024-12-23 13:35:14 -0800

    Version update
    Revised html & script

--- script.js
+++ script.js
@@ -1,14 +1,44 @@
+const btnQuote = document.getElementById("btn");
 const quote = document.getElementById("quote");
 const author = document.getElementById("author");
-const api_url = "https://api.api-ninjas.com/v1/quotes?category=business"
-    ;
+//const api_url = "https://api.api-ninjas.com/v1/quotes?category=business"
+;
 
-const apiKey ="4ABGFYmxxoYFk3yb8SAvRA==QlHzlw0nAG3emHwc"
+const apiKey = "4ABGFYmxxoYFk3yb8SAvRA==QlHzlw0nAG3emHwc";
 
+const options = {
+    method: "GET",
+    headers: {
+        "X-Api-Key": apiKey,
+    },
+};
 
-function getQuote() {
-    console.log("clicked");
+const apiURL = "https://api.api-ninjas.com/v1/quotes?category=business"
 
+async function getQuote() {
+
+    try {quote.innerText = "loading...";
+        btnQuote.disabled = true;
+        btnQuote.innerText = "Searching...";
+        const response = await fetch(apiURL, options);
+        const data = await response.json();
+    
+        btnQuote.disabled = false;
+        btnQuote.innerText = "New Quote";    
+ 
+        quote.innerText = data[0].quote;
+        author.innerText = data[0].author;
+        
+    } catch (error) {
+        quote.innerText = "Something went wrong, please try again later";
+        btnQuote.disabled = false;
+        btnQuote.innerText = "New Quote";
+        console.log(error);
+    }
 }
+function tweet() {
+    window.open("https://twitter.com/intent/tweet?text=" + quote.innerText + "---- by " + author.innerText, "Tweet Window", "width=600, height=300");
+}
+    
 
-quote.addEventListener("click", getQuote)
+btnQuote.addEventListener("click", getQuote);
